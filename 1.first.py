4fd87c0492c0726cb57049920300b29b00a04132
chaudharypoojabcg <43261021+chaudharypoojabcg@users.noreply.github.com>
2018-09-17 12:42:54 +0530

    Fourth Committ

--- 1.first.py
+++ 1.first.py
@@ -21,6 +21,10 @@ print("Sum of two numbers is {}".format(x+y))
 #creating pandas DataFrame
 import pandas as pd
 
+ab = pd.DataFrame()
+if ab.shape[0]== 0:
+	print("Empty")
+
 data=pd.DataFrame.from_dict(data={'A':{0:1,1:2,2:3},
                              'B':{0:3,1:4,2:5},
                              'C':{0:7,1:8,2:9}
